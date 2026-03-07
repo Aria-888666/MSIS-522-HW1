175165d8104ca5c364a466700da513e975390f6d
Aria-888666 <146395203+Aria-888666@users.noreply.github.com>
2026-03-07 09:38:02 -0800

    Rename variable X_transformed to X_processed

--- shap_analysis.py
+++ shap_analysis.py
@@ -1,6 +1,6 @@
 import shap
 import joblib
-import matplotlib.pyplot as plt
+
 from preprocessing import load_data, split_data
 
 df = load_data()
@@ -11,11 +11,11 @@ model = joblib.load("models/random_forest.pkl")
 
 X_sample = X_test.sample(200, random_state=42)
 
-X_transformed = model.named_steps["prep"].transform(X_sample)
+X_processed = model.named_steps["prep"].transform(X_sample)
 
 explainer = shap.Explainer(model.named_steps["model"])
 
-shap_values = explainer(X_transformed)
+shap_values = explainer(X_processed)
 
 shap.summary_plot(shap_values)
 
